34a968b2e310b7fd70bf347916f104d25a4861e6
chanhi2000 <chanhi2000@gmail.com>
2017-05-10 00:14:17 +0900

    mod01: dagger GithubServiceModule created

---
+++ app/src/main/java/com/markiiimark/daggertutorial/network/GithubServiceModule.java
@@ -0,0 +1,43 @@
+package com.markiiimark.daggertutorial.network;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import org.joda.time.DateTime;
+
+import javax.inject.Named;
+
+import dagger.Module;
+import dagger.Provides;
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by MarkiiimarK on 5/9/17.
+ */
+
+@Module(includes = {NetworkModule.class})
+public class GithubServiceModule {
+
+    @Provides
+    public GithubService provideGithubService(Retrofit githubRetrofit) {
+        return githubRetrofit.create(GithubService.class);
+    }
+
+    @Provides
+    public Gson provideGson() {
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        gsonBuilder.registerTypeAdapter(DateTime.class, new DateTimeConverter());
+        return gsonBuilder.create();
+    }
+
+    @Provides
+    public Retrofit provideRetrofit(OkHttpClient okHttpClient, Gson gson) {
+        return new Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .client(okHttpClient)
+                .baseUrl("https://api.github.com/")
+                .build();
+    }
+}
