8347e2a2a79e242fe7b5c6ee96b339d57121b359
chanhi2000 <chanhi2000@gmail.com>
2017-05-10 00:13:03 +0900

    mod01: dagger contextmodule created

---
+++ app/src/main/java/com/markiiimark/daggertutorial/ContextModule.java
@@ -0,0 +1,22 @@
+package com.markiiimark.daggertutorial;
+
+import android.content.Context;
+
+import dagger.Module;
+import dagger.Provides;
+
+/**
+ * Created by MarkiiimarK on 5/9/17.
+ */
+
+@Module
+public class ContextModule {
+
+    private final Context context;
+    public ContextModule(Context context) {  this.context = context;  }
+
+    @Provides
+    public Context provideContext() {
+        return context;
+    }
+}
